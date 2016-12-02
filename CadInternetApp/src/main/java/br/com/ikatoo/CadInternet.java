7497952337be341494581653e59146065345b262
Milton Carlos Katoo <mckatoo@gmail.com>
2016-12-02 13:11:11 -0200

    Inicial

---
+++ CadInternetApp/src/main/java/br/com/ikatoo/CadInternet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ikatoo;
+
+import br.com.ikatoo.views.FormLogin;
+
+/**
+ *
+ * @author Milton Carlos Katoo
+ */
+public class CadInternet {
+    public static void main(String[] args) {
+        FormLogin frmlogin = new FormLogin();
+        frmlogin.setVisible(true);
+    }
+}
